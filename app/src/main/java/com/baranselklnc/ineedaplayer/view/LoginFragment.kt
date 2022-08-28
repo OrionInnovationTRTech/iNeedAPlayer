8b74938f5664079c2575bdc4e3afce13d5de59bd
Baransel <49001016+baranselklnc@users.noreply.github.com>
2022-08-28 21:53:37 +0300

    lobby fragment created

--- app/src/main/java/com/baranselklnc/ineedaplayer/view/LoginFragment.kt
+++ app/src/main/java/com/baranselklnc/ineedaplayer/view/LoginFragment.kt
@@ -1,4 +1,4 @@
-package com.baranselklnc.ineedaplayer.auth
+package com.baranselklnc.ineedaplayer.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/baranselklnc/ineedaplayer/view/MainActivity.kt
+++ app/src/main/java/com/baranselklnc/ineedaplayer/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.baranselklnc.ineedaplayer
+package com.baranselklnc.ineedaplayer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baranselklnc.ineedaplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
